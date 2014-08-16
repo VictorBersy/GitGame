c3acaf770fb2b7c15b6ab3b5c330fe7ff1e5e870
Victor Bersy <victor.bersy@gmail.com>
2014-08-16 18:54:59 +0200

    add basic auth with github

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'high_voltage/pages#show', id: 'home'
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
