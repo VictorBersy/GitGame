9738359d09b04a8bd97efa6e24bbf2c774805785
Victor Bersy <victor.bersy@gmail.com>
2014-11-28 23:13:37 +0100

    add github_user_id to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,13 @@ class User < ActiveRecord::Base
 
   def self.from_omniauth(auth)
     where(auth.slice(:provider, :uid)).first_or_create do |user|
-      user.email        = auth.info.email
-      user.password     = Devise.friendly_token[0,20]
-      user.screen_name  = auth.info.nickname
-      user.username     = auth.info.nickname.parameterize
-      user.image        = auth.info.image
-      user.github_token = auth.credentials.token
+      user.email          = auth.info.email
+      user.password       = Devise.friendly_token[0,20]
+      user.screen_name    = auth.info.nickname
+      user.username       = auth.info.nickname.parameterize
+      user.image          = auth.info.image
+      user.github_token   = auth.credentials.token
+      user.github_user_id = auth.uid
     end
   end
 
